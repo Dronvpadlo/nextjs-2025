362916273bb7fc451c4266a6b116e12386a1a5ba
User <doronyuk714@mail.com>
2025-07-16 22:04:14 +0300

    lesson 1.3 commit

---
+++ src/app/(public)/users/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+
+type Props = {
+    params: {id:string}
+}
+const UserPage:FC<Props> = async ({params}) => {
+    const {id} = await params;
+
+
+    return (
+        <div>
+            User page {id}
+        </div>
+    );
+};
+
+export default UserPage;
---
+++ src/app/(public)/users/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UsersPage = () => {
+    return (
+        <div>
+            UsersPage content
+        </div>
+    );
+};
+
+export default UsersPage;
